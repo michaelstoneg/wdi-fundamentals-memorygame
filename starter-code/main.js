82468abdb22bd06ed57cc27c8e18bc9b1a3c04c1
michael grandison <michaelgrandison@michaels-air.home>
2016-09-12 19:43:53 +0100

    added match alert

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,13 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	window.alert ("You found a match");
+}
+else if (cardOne === cardThree) {
+	window.alert ("You found a match");
+}
+else {
+	window.alert ("Sorry, try again");
+}
