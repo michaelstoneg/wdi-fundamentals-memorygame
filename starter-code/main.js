0397508de97d21da918e0a4d001ef5d8eb54b099
michael grandison <michaelgrandison@michaels-MacBook-Air.local>
2016-09-15 00:33:49 +0100

    added createBoard func

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,12 +3,30 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {
-	window.alert ("You found a match");
-}
-else if (cardOne === cardThree) {
-	window.alert ("You found a match");
-}
-else {
-	window.alert ("Sorry, try again");
+//if (cardTwo === cardFour) {
+	//window.alert ("You found a match");
+//}
+//else if (cardOne === cardThree) {
+	//window.alert ("You found a match");
+//}
+//else {
+//	window.alert ("Sorry, try again");
+//}
+var board = document.getElementById('game-board');
+
+// function that will create your board
+function createBoard() {
+  // loop through your cards array to create card elements for your board
+  for (var i=0; i<4; i++) {
+
+    // create a div element which will be used as a card
+    var cards = document.createElement('div');
+
+    // add a class to the card element which will help link styling
+    cards.className = 'card';
+
+    // append the card to the board
+   board.appendChild(cards);
+  }
+
 }
