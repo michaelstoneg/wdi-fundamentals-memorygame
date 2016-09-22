ad08810143dd713517646dbc126f1b6081cb8275
michael grandison <michaelgrandison@michaels-MacBook-Air.local>
2016-09-22 13:59:12 +0100

    test

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,27 +1,38 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-
-//if (cardTwo === cardFour) {
+/*
+if (cardTwo === cardFour) {
 	//window.alert ("You found a match");
-//}
-//else if (cardOne === cardThree) {
-	//window.alert ("You found a match");
-//}
-//else {
-//	window.alert ("Sorry, try again");
-//}
+}
+else if (cardOne === cardThree) {
+	window.alert ("You found a match");
+}
+else {
+	window.alert ("Sorry, try again");
+}
+var boardElement = document.querySelector('#game-board');
+*/
 var boardElement = document.getElementById("game-board");
-//var footerElement = document.getElementById("footer");
-//function that will create your board
-//function createBoard() {
-//	var boardElement = document.createElement('div');
-//	boardElement.idName = "game-board";
-//	boardElement.className = "board";
-//	document.body.insertBefore(boardElement, footerElement);
-//}
-
+console.log(boardElement);
+/*var footerElement = document.getElementById("footer");
+function that will create your board
+function createBoard() {
+	var boardElement = document.createElement('div');
+	boardElement.idName = "game-board";
+	boardElement.className = "board";
+	document.body.insertBefore(boardElement, footerElement);
+}
+for (var i=0; i<cards.length; i++) {
+ when a card is clicked the function isTwoCards will be executed
+    cardElement.addEventListener('click', isTwoCards)
+}*/
 function createCards() {
   // loop through your cards array to create card elements for your board
+  //var i = 0;
+  //while(i<4) {
+    //stuff
+    //i++;
+  //}
   for (var i=0; i<cards.length; i++) {
 
     // create a div element which will be used as a card
@@ -34,23 +45,22 @@ function createCards() {
    boardElement.appendChild(cardElements);
   //}
   //for (var i = 0; i< cards.length; i++) {
-		cardElements.setAttribute('data-card', cards[i]);
-		cardElements.addEventListener('click', isTwoCards, cardImages);
-	}
+    cardElements.setAttribute('data-card', cards[i]);
+    cardElements.addEventListener('click', cardImages);
+    console.log("cards away");
+  }
 }
-
-//for (var i=0; i<cards.length; i++) {
-// when a card is clicked the function isTwoCards will be executed
-//    cardElement.addEventListener('click', isTwoCards)
-//}
+createCards();
 function cardImages() {
-	if (document.getAttribute('data-card') === 'king') {
+	if (this.getAttribute('data-card') === 'king') {
 		this.innerHTML = '<img src="images/king.png">';
-	}
-	else if (document.getAttribute('data-card') === 'queen') {
-		this.innerHTML = '<img src="images/queen.pgn">';
+    //this.style.background-image = '<img src="images/king.png">';
+  }
+	else {
+		this.innerHTML = '<img src="images/queen.png">';
 	}
 }
+cardImages();
 //checks to see if there are cards in play
 function isTwoCards() {
   cardsInPlay.push(this.getAttribute('data-card'));
